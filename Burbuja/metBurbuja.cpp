08dca939ccfdd601e10f2710ad2bacb198445241
JMMendozaF <jmmendozaf@unjbg.edu.pe>
2025-10-22 00:47:20 -0500

    Se modificó y agregó colores al método Burbuja

--- Burbuja/metBurbuja.cpp
+++ Burbuja/metBurbuja.cpp
@@ -10,8 +10,10 @@ int main (){
 	HANDLE hConsole=GetStdHandle(STD_OUTPUT_HANDLE);
 	srand(time(NULL));
 	
+	color (hConsole, 6);
 	cout<<"--- MÉTODO DE ORDENAMIENTO 'BURBUJA' ---\n";
 	int tam;
+	color (hConsole, 7);
 	cout<<"Tamaño del arreglo: ";
 	cin>>tam;
 	int arr[tam];
@@ -20,11 +22,14 @@ int main (){
 		arr[i]=rand()%9;
 	}
 	
+	color (hConsole, 3);
 	cout<<"-- ARREGLO INICIAL --\n";
+	color (hConsole, 7);
 	for (int i=0; i<tam; i++){
 		cout<<arr[i]<<" ";
 	}
 	
+	//Ordenar el arreglo
 	int aux;
 	for (int i=0; i<tam-1; i++){
 		for (int j=0; j<tam-1; j++){
@@ -36,13 +41,19 @@ int main (){
 		}
 	}
 	
+	color (hConsole, 3);
 	cout<<"\n- ORDEN ASCENDENTE -\n";
+	color (hConsole, 7);
 	for (int i=0; i<tam; i++){
 		cout<<arr[i]<<" ";
 	}
+	color (hConsole, 3);
 	cout<<"\n- ORDEN DESCENTE -\n";
+	color (hConsole, 7);
 	for (int i=tam-1; i>=0; i--){
 		cout<<arr[i]<<" ";
 	}
+	
+	color (hConsole, 7);
 	return 0;
 }
