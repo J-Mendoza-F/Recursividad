e8bb218ec14b4afe9c12e39a8118137373a028cc
JMMendozaF <jmmendozaf@unjbg.edu.pe>
2025-10-22 00:05:53 -0500

    Colores al factorial y código de colores

---
+++ Colores/Colores.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <windows.h>
+
+#define color SetConsoleTextAttribute
+using namespace std;
+
+int main (){
+	HANDLE hConsole=GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleOutputCP(CP_UTF8);
+	for (int i=0; i<15; i++){
+		color(hConsole, i);
+		cout<<i<<". Hola mundo\n";
+	}
+	return 0;
+}
--- Factorial/factorial.cpp
+++ Factorial/factorial.cpp
@@ -1,14 +1,26 @@
 #include <iostream>
 #include <windows.h>
+
+#define color SetConsoleTextAttribute
 using namespace std;
 
 int fact(int);
 
 int main (){
+	HANDLE hConsole=GetStdHandle(STD_OUTPUT_HANDLE);
 	SetConsoleOutputCP(CP_UTF8);
+	
+	color(hConsole, 6);
+	cout<<"--- FACTORIAL DE UN NÚMERO ---\n";
+	color(hConsole, 3);
+	cout<<"(Método recursivo)\n";
     int n;
+    color(hConsole, 7);
     cout<<"Ingrese un número: ";
     cin>>n;
+    color(hConsole, 6);
+    cout<<"--- RESULTADOS ---\n";
+    color(hConsole, 7);
     cout<<"El factorial de "<<n<<" es: "<<fact(n)<<endl;
 
     return 0;
